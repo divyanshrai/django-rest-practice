409afdc7604df6ee63145eca8985a87c48193dc6
divyanshrai <divyansh.rai.1999@gmail.com>
2020-08-07 06:52:23 +0530

    Added apiview decorator

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -6,7 +6,8 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Article
-        fields = ['id', 'title', 'author']
+        #fields = ['id', 'title', 'author','email']
+        fields = '__all__'
 
 ''' class ArticleSerializer(serializers.Serializer):
     
