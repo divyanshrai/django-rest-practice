7e3a9cdddabbe7b10deff00624ccd8e0f04410fd
divyanshrai <divyansh.rai.1999@gmail.com>
2020-08-08 03:19:21 +0530

    A normal viewset addded and router linked

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,19 @@
 
-from django.urls import path
+from django.urls import path, include
 #from .views import article_list, article_detail
 from .views import ArticleAPIView, ArticleDetails
 from .views import GenericAPIView
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet,
+                basename='article')
+
 urlpatterns = [
+    path('viewset/', include(router.urls)),
+
+
     #path('article/', article_list), #api view
     path('article/', ArticleAPIView.as_view()), #class based api views 
     #path('detail/<int:pk>/', article_detail), #api view
